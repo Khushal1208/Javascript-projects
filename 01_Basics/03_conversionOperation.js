268ef04e0a2b71dd212b86035b7dcf4eb044ebcd
khushal parmar <khushalparmar1208@gmail.com>
2024-10-23 17:42:22 +0530

    container config done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33abc" //NaN
+// let score = null   // 0
+// let score = undefined   // NaN
+// let score = "Khushal"   // NaN
+// let score = 33 // 33
+// let score = "true" // 1
+let score = "false" // 0
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1 //true
+// let isLoggedIn = 0 // false
+// let isLoggedIn = ""  // false
+// let isLoggedIn = "hitesh" //true
+// let isLoggedIn = null //false
+// let isLoggedIn = undefined //false
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+ 
+
+
