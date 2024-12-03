1b9480f5de3f76b3178ffcd3d6d8c559cf054b62
khushal parmar <khushalparmar1208@gmail.com>
2024-12-03 23:26:02 +0530

    Resolved merge conflicts

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as new version
+
+// alert(3+3)    it is node not browser
+
+console.log(3+3)
+
+// datatypes
+/* 
+number => 2*53
+bigint => for large numbers
+string => "assemebly of characters"
+boolean => true or false
+null => standalone value , it is object type 
+undefined => kindda place holder for unassingned variable (type = undefined)
+symbole => unique
+*/
+
+// object 
+console.log(typeof "khushal")
+console.log(typeof undefined) // undefined it undefined
+console.log(typeof null)  // null is object type 
