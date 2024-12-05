2edf41be7c30a51e7ea4b238d90b86cc2079d780
khushal parmar <khushalparmar1208@gmail.com>
2024-12-06 00:40:00 +0530

    comparisions

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -5,17 +5,23 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);  //true
+// console.log("02" > 1); //true
+// // typescript don't allows to compare two diff datatypes 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// the reason is that an equality check == and comparisions >, <, >=, <= work diffrently
+// comparisions convert null to a number , treating it as 0
+// that's why 3rd null >= 0 is true and 1st null > 0 is false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
-// === 
 
-console.log("2" === 2);
+//  ===  not only value will be checked but the datatype also will be checked
+ 
+console.log("2" == 2); //true
+console.log("2" === 2); //false
