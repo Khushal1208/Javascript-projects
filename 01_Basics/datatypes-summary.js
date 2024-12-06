8654e426b3e9ae3d464bc965bef8b93279630bff
khushal parmar <khushalparmar1208@gmail.com>
2024-12-06 14:06:29 +0530

    datatype-summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -1,40 +1,67 @@
 //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// js is dynamically typed language
 
-const score = 100
-const scoreValue = 100.3
+const score = 100 // number category
+const scoreValue = 100.3  // number category
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const isLoggedIn = false // boolean type
+const outsideTemp = null // null type
+let userEmail; //undefined type
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123')  //uniqueness
+const anotherId = Symbol('123')  // unique 
 
-console.log(id === anotherId);
+console.log(id === anotherId); // false
+// both are not same 
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 345654357665435675n
 
 
 // stack and heap memory concept
+
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
+//Array's are defined like this 
 const heros = ["shaktiman", "naagraj", "doga"];
+
+// object's are defined like this
+// {} isme kuch bhi define kia to wo object banjayega
 let myObj = {
     name: "hitesh",
     age: 22,
 }
 
+//function's are defined like this
 const myFunction = function(){
     console.log("Hello world");
 }
 
+console.log(typeof number);
 console.log(typeof anotherId);
+console.log(typeof myFunction); //object function
+
+// jo bhi non premitive hote hai unka datatype object hi hota hai 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
-// thank you
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function object
+//        Object  =>  object
