874a0091f752d5af3463dff045d1503273046ffd
khushal parmar <khushalparmar1208@gmail.com>
2024-12-11 17:10:11 +0530

    Object de-structor and JSON API intro

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -21,10 +21,10 @@ const regularUser = {
 };
 
 // ACCESSING NESTED OBJECTS
-console.log(regularUser.fullname.userFullname.firstname); // "hitesh"
+// console.log(regularUser.fullname.userFullname.firstname); // "hitesh"
 
 // OPTIONAL CHAINING (Checking if an object or property exists)
-console.log(regularUser.fullname?.userFullname?.firstname); // "hitesh"
+// console.log(regularUser.fullname?.userFullname?.firstname); // "hitesh"
 
 // OBJECT ASSIGNMENT
 const obj1 = { 1: 'a', 2: 'b' };
@@ -33,10 +33,12 @@ const obj2 = { 3: 'a', 4: 'b' };
 // Incorrect method of combining objects
 // const obj3 = { obj1, obj2 };
 // Result: { obj1: { 1: 'a', 2: 'b' }, obj2: { 3: 'a', 4: 'b' } }
+// obj1 = obj3
 
 // Using Object.assign()
 const obj3 = Object.assign({}, obj1, obj2);
 // obj1 remains unchanged
+// obj1 != obj3
 // obj3 = { 1: 'a', 2: 'b', 3: 'a', 4: 'b' }
 
 // Using Spread Syntax
@@ -44,17 +46,17 @@ const obj4 = { ...obj1, ...obj2 };
 // obj4 = { 1: 'a', 2: 'b', 3: 'a', 4: 'b' }
 
 // ACCESSING KEYS, VALUES, AND ENTRIES
-console.log(Object.keys(tinderUser)); 
+// console.log(Object.keys(tinderUser)); 
 // Outputs: [ 'id', 'name', 'isLoggedIn' ]
 
-console.log(Object.values(tinderUser)); 
+// console.log(Object.values(tinderUser)); 
 // Outputs: [ '123abc', 'sammy', false ]
 
-console.log(Object.entries(tinderUser)); 
+// console.log(Object.entries(tinderUser)); 
 // Outputs: [ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isLoggedIn', false ] ]
 
 // CHECKING PROPERTY EXISTENCE
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); 
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); 
 // Outputs: true
 
 
@@ -70,3 +72,47 @@ const users = [
 ]
 
 users[1].email
+
+
+
+const course = {
+    coursename: 'js in hindi',
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+//OBJECT de-structuring
+const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+// both are same
+
+// console.log(instructor)
+
+// IN REACT
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "hitesh")
+
+
+// when you see any "{}" curly braces then it must be used for de-structering purposes
+
+// de-structering of array is also possible
+
+
+// JSON looks like this , both keys and values are in string format
+// object with no name is json file
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price" : "free"
+// }
+
+//ARRAY type of object
+// [
+//     {},
+//     {},
+//     {}
+// ]
