2eb3e04bc3fd0fe1ad340e7076e0080f7479a82a
khushal parmar <khushalparmar1208@gmail.com>
2024-12-13 09:44:48 +0530

    use of spread and rest in funcitons , and accessing objects , array via functions

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -68,6 +68,69 @@ function loginUserMassage3(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMassage3()) //sam just logged in
-console.log(loginUserMassage3("hitesh")) //hitesh just logged in 
+// console.log(loginUserMassage3()) //sam just logged in
+// console.log(loginUserMassage3("hitesh")) //hitesh just logged in 
+
+
+
+// Functions with objects and array
+
+// how to calculate cart total when user is constanly adding product in it ?
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2));
+
+// adding more prices 
+// console.log(calculateCartPrice(200,400,500)); // only 200 is returned 
+
+// Here comes the importance of spread and rest operator 
+
+function calculateCartPrice(...num1){
+    // return ...num1   wrong way
+        return num1   // right way
+    }
+    
+    // console.log(calculateCartPrice(200,400,500,6000)); 
+    // [ 200, 400, 500, 6000 ]
+
+
+function calculateCartPrice2(val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice2(200,400,500,6000));
+// [ 500, 6000 ]   val1 = 200 and val2 = 400 so only remaining numbers are printed.
+
+
+const user = {
+    username : "hitesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user) //Username is hitesh and price is 199
+// if by chance object key changed then it will print as undefined
+// js is not type safe but typescript is safe
+
+handleObject({
+    username:"sam" ,
+    price : 399
+}) 
+// Username is sam and price is 399
+
+const myNewArray = [200 ,400 ,100 , 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray)); // 400
+
+
 
